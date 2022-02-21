7328a15031143e640e5e7a4254e270d2ffa7e52d
Michael Trew <mtrew2015@gmail.com>
2022-02-21 12:00:25 -0500

    adding dotenv stuff

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+
 
 const app = express();
+const port = 5000
 
-app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = process.env.MONGO_URL
+
+// mongo db cloud atlas 
+
+app.listen(port, () => {
+    console.log(`app is running on port ${port}`)
+})
