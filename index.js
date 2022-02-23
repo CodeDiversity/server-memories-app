158b1dbf282fca949274eb76233515614be7f717
Michael Trew <mtrew2015@gmail.com>
2022-02-23 13:19:54 -0500

    adding host

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const app = express();
 
 const port = process.env.port || 5000;
 
+const host = '0.0.0.0';
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
@@ -26,6 +28,6 @@ const CONNECTION_URL = process.env.MONGO_URL;
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(port, () => console.log(`server running on port ${port}`))
+    app.listen(port, host, () => console.log(`server running on port ${port}`))
   )
   .catch((err) => console.log(err.message));
